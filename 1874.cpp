e588a919fc09acfef1d35a4f3ecec0d57c893583
Yongwoo Kim <keepingitflow@gmail.com>
2020-05-27 16:38:45 +0900

    Sovle No. 1874

--- 1874.cpp
+++ 1874.cpp
@@ -3,41 +3,12 @@
 #include<queue>
 
 using namespace std;
-stack<int> s;
-queue<int> import;
-int n;
-
-void go(int i) {
-cout<<"--------"<<i<<"--------\n";
-
-    if(s.empty()==1) {
-        s.push(i);
-        cout<<"+"<<"\n";
-        return;
-    } else if(import.front()==s.top()) {
-        s.pop();
-        import.pop();
-        cout<<"-"<<"\n";
-        return go(i);
-    } else {
-        s.push(i);
-        cout<<"+"<<"\n";
-        return;
-    } 
-
-    if(i==n && s.empty()==0 && import.empty()==0) {
-        cout<<"+++++++++++++"<<"\n";
-        if(import.front()==s.top()) {
-            return go(i);
-        } else {
-            cout<<"NO"<<"\n";
-            return;
-        }
-    }
-
-}
 
 int main() {
+    int n;
+    stack<int> s;
+    queue<int> import;
+    queue<string> ans;
     cin>>n;
 
     for(int i=0; i<n; i++) {
@@ -46,7 +17,34 @@ int main() {
         import.push(tmp);
     }
 
-    for(int i=1; i<=n; i++) {
-        go(i);
+    int i=1;
+    while(import.empty()==0) {
+        if(s.empty()==1) {
+            s.push(i);
+            ans.push("+");
+            i++;
+        } else if(s.top() == import.front()) {
+            s.pop();
+            import.pop();
+            ans.push("-");
+        } else if(s.top() != import.front()) {
+            if(i<=n) {
+                s.push(i);
+                ans.push("+");
+                i++;
+            } else {
+                break;
+            }
+        } 
     }
+
+    if(import.empty()==1) {
+        while(ans.empty()==0) {
+            cout<<ans.front()<<"\n";
+            ans.pop();
+        }
+    } else {
+        cout<<"NO"<<"\n";
+    }
+    
 }
