7dc85de47047f207bdf55115b94bba536fb697f8
Yongwoo Kim <keepingitflow@gmail.com>
2020-05-31 20:28:48 +0900

    I can't understand why my solution is incorrect

--- 1212.cpp
+++ 1212.cpp
@@ -4,30 +4,6 @@
 using namespace std;
 stack<string> s;
 
-void go(int n) {
-    int c = 0;
-    while(1) {
-        if(n/2 == 1) { 
-            if(n%2 == 0) {
-                cout<<"push! - 0\n";
-                s.push("0");
-                n /= 2;
-            }
-            else {
-                s.push("1");
-                cout<<"push! - 1\n";
-                n /= 2;
-            }
-        }
-        else { // 1 0
-            cout<<"push! - 1\n";
-            s.push("1"); 
-            cout<<"Finish go\n";
-            break;
-        }
-    }
-}
-
 int main() {
     int inp;
 
@@ -36,20 +12,26 @@ int main() {
     while(1) {
         if(inp==0) break;
         if(inp%10 > 0) {
-            cout<<"go! - inp : "<<inp<<"\n";
-            go(inp%10);
+            for(int i=0; i<3; i++) {
+                if(n%2==0) {
+                    s.push("0");
+                    n /= 2;
+                } else {
+                    s.push("1");
+                    n /= 2;
+                }
+            }
         }
         else {
-            cout<<"push! - 000\n";
             s.push("000");
         }
         inp /= 10;
     }
     
-    cout<<s.top()<<"\n"
-
-    for(int i=0; i<s.size(); i++) {
-        cout<<s.top();
+    int max = s.size();
+    for(int i=0; i<max; i++) {
+        if(s.top() == "0" && i<2) {}
+        else cout<<s.top();
         s.pop();
     }
 }
--- 1373.cpp
+++ 1373.cpp
@@ -1,31 +1,21 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
     string s;
-
-    cin>> s;
-
-    int n = s.size();
-
-    cout<<"s.size() : "<< n<<"\n";
+    cin >> s;
     
-    if (n%3 == 1) {
-        cout<<"11111111"<<"\n";
+    int n = s.size();
+    
+    if (n%3 == 1)
         cout << s[0];
-    } else if (n%3 == 2) {
-        cout<<"22222222"<<"\n";
-        cout<<"s[0] : "<<s[0]<< " / s[0] - '0' : " << s[0]-'0' << " / s[1] : " <<s[1]<<"\n"; 
-        cout << (s[0])*2 + (s[1]);
-    }
-
-    cout<<"\n"<<"\n";
+    else if (n%3 == 2)
+        cout << (s[0]-'0')*2 + (s[1]-'0');
 
     for (int i=n%3; i<n; i+=3)
         cout << (s[i]-'0')*4 + (s[i+1]-'0')*2 + (s[i+2]-'0');
 
     cout << '\n';
-    
 }
