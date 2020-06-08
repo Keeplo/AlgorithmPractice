2db6ec2dd1bf3268177de23f0e8329039885deaa
YongwooKim <keepingitflow@gmail.com>
2020-06-08 22:48:55 +0900

    Try No.9093

--- 9093.cpp
+++ 9093.cpp
@@ -8,13 +8,15 @@ int main() {
     int n;
 
     cin>>n;
+    cin.ignore(128,'\n');
 
+    //* 어떤 데이터형(int or char)과 string형을 연달아 입력받을 때 getline 버퍼에 입력된 줄바꿈 문자를 받아들여 버리는데 이를 막기위해서 사이에 getchar()를 삽입해줄 수 있다.
+    //https://leeusin.tistory.com/418
+    
     string s[n];
 
-    cout<<"----"<<n<<"----\n";
-
     for(int i=0; i<n; i++) {
-        
+
         getline(cin, s[i]);
 
         stack<string> stk;
