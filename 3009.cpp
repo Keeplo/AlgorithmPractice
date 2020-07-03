3728074e3eae18e4d0fea79e4c7fc34d8db5d770
YongwooKim <keepingitflow@gmail.com>
2020-07-04 00:34:28 +0900

    docs: Complete No.3009

--- 3009.cpp
+++ 3009.cpp
@@ -12,14 +12,14 @@ void solution(vector<vector<int>> v) {
         ans[1] ^= v[i][1];
     }
 
-    cout<<"[" << ans[0] << ", "<<ans[1]<<"]\n";
+    cout<< ans[0] << " "<<ans[1]<<"\n";
 }
 
 int main() {
     int n;
     vector<vector<int>> vvv;
     vector<int> vv;
-    for(int i=0; i<6; i++) {
+    for(int i=1; i<=6; i++) {
         cin>>n;
         if(i%2==1) {
             vv.push_back(n);
