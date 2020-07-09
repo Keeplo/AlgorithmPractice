34af36cd42b7ae0d4e7af70e2d6a016e476ca9f7
YongwooKim <keepingitflow@gmail.com>
2020-07-09 16:21:18 +0900

    docs: 'For Loop
    ' 해결

---
+++ hackerrank/cpp_05.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+struct Alpha {
+    string s[9]; 
+};
+
+int main() {
+    int a,b;
+    struct Alpha alphabet = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    cin>>a>>b;
+
+    for(int i=a; i<=b; i++) {
+        if(i<=9) {
+            cout<< alphabet.s[i-1]<<"\n";
+        } else {
+            if(i%2==0) {
+                cout<<"even\n";
+            } else {
+                cout<<"odd\n";
+            }
+        }
+    }
+    return 0;
+}
