93336607042b824a0468b938070b3333ab634088
YongwooKim <keepingitflow@gmail.com>
2020-07-09 16:38:46 +0900

    docs: 'Functions
    ' 문제 풀이완료

---
+++ hackerrank/cpp_06.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int max_of_four(int a, int b, int c, int d) {
+    int ans=0, arr[4];
+
+    arr[0] = a;
+    arr[1] = b;
+    arr[2] = c;
+    arr[3] = d;
+
+    for(int i=0; i<4; i++) {
+        if(arr[i]>ans)
+            ans = arr[i];
+    }
+
+    return ans;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
