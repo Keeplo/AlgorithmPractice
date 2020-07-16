c7f74d61f37286958e992c92d82f024d0818ff89
YongwooKim <keepingitflow@gmail.com>
2020-07-16 21:52:42 +0900

    docs: Hackerrank Day06 파일 생성 및 탑 문제 수정

---
+++ hackerrank/SwiftVersion/Day06/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day06
+//
+//  Created by Yongwoo Marco on 2020/07/16.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
