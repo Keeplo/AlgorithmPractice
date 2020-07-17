d0185e10df93af6b13fefb242a9e1ceac2df008f
YongwooKim <keepingitflow@gmail.com>
2020-07-17 21:28:57 +0900

    docs: Day07 문제풀이완료

--- hackerrank/SwiftVersion/Day07/main.swift
+++ hackerrank/SwiftVersion/Day07/main.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
-print("Hello, World!")
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arrTemp = readLine() else { fatalError("Bad input") }
+
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+
+
+var answer: String = ""
+
+for item in arr {
+    answer = String(item) + " " + answer
+}
+
+print(answer)
 
--- hackerrank/cpp_10.cpp
+++ hackerrank/cpp_10.cpp
@@ -10,29 +10,18 @@ vector<int> parseInts(string str) {
 	// Complete this function
     stringstream ss(str);
     vector<int> answer;
-
+    
     int tmp;
     char ch;
-
-    ss>>tmp;
-    answer.push_back(tmp);
-    cout<<"ss : " << ss.str() << " / ss.tellp : " <<<ss.tellp()<< " / tmp : "<<tmp<<"\n";
-
-int count = 0;
-    while(1) {
-        if(ss.str().find(",") != string::npos) {
-            ss>>ch;
+    for(int i=0; i<str.size(); i++) {
+        if(str[i]==',') {
             ss>>tmp;
             answer.push_back(tmp);
-            cout<<"ss : " << ss.str() << " / tmp : "<<tmp<<"\n";
-            if(count==3) break;
-            //if(ss.seekg.cur() == ss.seekg.end()) break;
-        } else {
-            cout<<"break while\n";
-            break;
+            ss>>ch;
         }
-        count++;
     }
+    ss>>tmp;
+    answer.push_back(tmp);
 
     return answer;
 }
