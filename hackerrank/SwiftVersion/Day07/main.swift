766fa7a0d98f50d19990401d1557386acef8d7ff
YongwooKim <keepingitflow@gmail.com>
2020-07-17 21:28:21 +0900

    docs: Day07 Swift 문제풀이 완료

---
+++ hackerrank/SwiftVersion/Day07/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day07
+//
+//  Created by Yongwoo Marco on 2020/07/17.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
