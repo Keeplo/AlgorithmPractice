6ce2e2b556bb498dcf9aef000fb20fd23521f8f5
YongwooKim <keepingitflow@gmail.com>
2020-07-27 15:57:38 +0900

    docs: 프로젝트 생성

--- programmers/Programmers/Stack/Queue_printer/main.swift
+++ programmers/Programmers/Stack/Queue_printer/main.swift
@@ -8,13 +8,14 @@
 
 import Foundation
 
-
-
+let priorities = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+guard let location = readLine() else { return }
 
 func solution(_ priorities:[Int], _ location:Int) -> Int {
     var count=0, location=0
     
-    
-    
-    return 0
+    return count
 }
+
+print(solution(priorities, location))
+
