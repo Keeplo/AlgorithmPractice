590d2f356ed405185d3ede27ccd0bdb41a750706
YongwooKim <keepingitflow@gmail.com>
2020-07-27 16:47:42 +0900

    docs: 프로그래머스 기능개발 파일 생성

---
+++ programmers/Programmers/Stack/Queue_processDevelop/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Stack/Queue_processDevelop
+//
+//  Created by Yongwoo Marco on 2020/07/27.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+let priorities = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+let location = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+
+func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
+    
+    return []
+}
