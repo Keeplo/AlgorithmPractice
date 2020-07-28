ea9b8de2c84fb1da4b2565d144b0fa7001ef97e3
YongwooKim <keepingitflow@gmail.com>
2020-07-28 19:38:21 +0900

    docs: 해커랭크 18일차 C++ 문제풀이완료

---
+++ hackerrank/day18.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+    public:
+        stack<char> s;
+        queue<char> q;
+    
+    void pushCharacter(char c) {
+        this->s.push(c);
+    }
+    void enqueueCharacter(char c) {
+        this->q.push(c);
+    }
+    char popCharacter() {
+        char popChar;
+        popChar = s.top();
+        s.pop();
+        return popChar;
+    }
+    char dequeueCharacter() {
+        char dequeueChar;
+        dequeueChar = q.front();
+        q.pop();
+        return dequeueChar;
+    }
+};
+
+int main() {
+    string s;
+    getline(cin, s);
+    
+    Solution obj;
+    
+    for (int i = 0; i < s.length(); i++) {
+        obj.pushCharacter(s[i]);
+        obj.enqueueCharacter(s[i]);
+    }
+    
+    bool isPalindrome = true;
+    
+    for (int i = 0; i < s.length() / 2; i++) {
+        if (obj.popCharacter() != obj.dequeueCharacter()) {
+            isPalindrome = false;
+            
+            break;
+        }
+    }
+    
+    if (isPalindrome) {
+        cout << "The word, " << s << ", is a palindrome.";
+    } else {
+        cout << "The word, " << s << ", is not a palindrome.";
+    }
+    
+    return 0;
+}
