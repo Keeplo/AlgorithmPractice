5bc33c55ba5dd8b331fc3b876cc1ef741fdb41be
YongwooKim <keepingitflow@gmail.com>
2020-07-28 21:55:59 +0900

    docs: files 수정

--- hackerrank/SwiftVersion/Day18/main.swift
+++ hackerrank/SwiftVersion/Day18/main.swift
@@ -8,5 +8,53 @@
 
 import Foundation
 
-print("Hello, World!")
+class Solution {
+    var s = [Character]()
+    var q = [Character]()
+    
+    func pushCharacter(ch: Character) {
+        self.s.append(ch)
+    }
+    func enqueueCharacter(ch: Character) {
+        self.q.append(ch)
+    }
+    func popCharacter() -> Character {
+        let data = self.s[s.count-1]
+        self.s.remove(at: self.s.count-1)
+        return data
+    }
+    func dequeueCharacter() -> Character {
+        let data = self.q[0]
+        self.q.remove(at: 0)
+        return data
+    }
+}
 
+// read the string s.
+let s = readLine()!
+
+// create the Solution class object p.
+let obj = Solution()
+
+// push/enqueue all the characters of string s to stack.
+for character in s {
+    obj.pushCharacter(ch: character)
+    obj.enqueueCharacter(ch: character)
+}
+
+
+var isPalindrome = true
+
+for _ in 0..<(s.count / 2) {
+    if obj.popCharacter() != obj.dequeueCharacter() {
+        isPalindrome = false
+
+        break
+    }
+}
+
+if isPalindrome {
+    print("The word, \(s), is a palindrome.")
+} else {
+    print("The word, \(s), is not a palindrome.")
+}
