39c6e3d1fba3a0cfe326121b55ea4aea136b9114
YongwooKim <keepingitflow@gmail.com>
2020-07-31 15:23:32 +0900

    docs: Day21 스위프트 버전 파일생성

---
+++ hackerrank/SwiftVersion/Day21/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day21
+//
+//  Created by Yongwoo Marco on 2020/07/31.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
