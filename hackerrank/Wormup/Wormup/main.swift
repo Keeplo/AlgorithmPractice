e02c2b4a34703aecc5f94b1ea6b5ad5058834ad3
YongwooKim <keepingitflow@gmail.com>
2020-08-01 19:13:03 +0900

    docs: Hackerrank.Wormup 프로젝트 생성

---
+++ hackerrank/Wormup/Wormup/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Wormup
+//
+//  Created by Yongwoo Marco on 2020/08/01.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
