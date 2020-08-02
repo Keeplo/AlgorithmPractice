63a38a378114cffb6533988d8e94bac05edc0783
YongwooKim <keepingitflow@gmail.com>
2020-08-02 21:42:39 +0900

    dosc: Solved Hackerrank Day23.swift

---
+++ hackerrank/SwiftVersion/Day23/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day23
+//
+//  Created by Yongwoo Marco on 2020/08/02.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
