218aa74402f5dedf545fe3af434fedfabe9bc890
YongwooKim <keepingitflow@gmail.com>
2020-08-03 15:44:55 +0900

    docs:Solved Hackerrank/30DaysOfCode/Day24.swift

---
+++ hackerrank/SwiftVersion/Day24/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day24
+//
+//  Created by Yongwoo Marco on 2020/08/03.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
