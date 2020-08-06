64ffac7e745823612d863bd5a33f0ffee98b9ebb
YongwooKim <keepingitflow@gmail.com>
2020-08-06 17:21:50 +0900

    docs: Solved Hackerrank/Wormup/SimpleArraySum

--- hackerrank/Wormup/SimpleArraySum/main.swift
+++ hackerrank/Wormup/SimpleArraySum/main.swift
@@ -25,16 +25,22 @@ FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
 let fileHandle = FileHandle(forWritingAtPath: stdout)!
 */
 
+// 문자 입력 (정수)
 guard let arCount = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
 else { fatalError("Bad input") }
 
+
+// 문자열 받기
 guard let arTemp = readLine() else { fatalError("Bad input") }
+
+// 문자열 " " 로 나누어 배열로 만들기
 let ar: [Int] = arTemp.split(separator: " ").map {
     if let arItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
         return arItem
     } else { fatalError("Bad input") }
 }
 
+// 문자열 갯수 확인
 guard ar.count == arCount else { fatalError("Bad input") }
 
 let result = simpleArraySum(ar: ar)
