2ad7f3d17aefa6bb3b9782afd8671180b98a331c
YongwooKim <keepingitflow@gmail.com>
2020-08-07 15:41:05 +0900

    docs: Changed folder name

---
+++ hackerrank/Warmup/DiagonalDifference/main.swift
@@ -0,0 +1,64 @@
+//
+//  main.swift
+//  DiagonalDifference
+//
+//  Created by Yongwoo Marco on 2020/08/06.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+func diagonalDifference(arr: [[Int]]) -> Int {
+    var result: Int = 0
+    let lastIndex = arr.count-1
+
+    for i in 0...lastIndex {
+        result += (arr[i][i] - arr[i][lastIndex-i])
+    }
+
+    result = abs(result)
+    return result
+}
+
+/*
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+*/
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+var arr = [[Int]]()
+
+for _ in 1...n {
+    guard let arrRowTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+    let arrRow: [Int] = arrRowTemp.split(separator: " ").map {
+        if let arrItem = Int($0) {
+            return arrItem
+        } else { fatalError("Bad input") }
+    }
+
+    guard arrRow.count == n else { fatalError("Bad input") }
+
+    arr.append(arrRow)
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+let result = diagonalDifference(arr: arr)
+
+print(result)
+
+/*
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
+*/
---
+++ hackerrank/Warmup/SimpleArraySum/main.swift
@@ -0,0 +1,53 @@
+//
+//  main.swift
+//  SimpleArraySum
+//
+//  Created by Yongwoo Marco on 2020/08/01.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+// https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+import Foundation
+
+func simpleArraySum(ar: [Int]) -> Int {
+    var result = 0
+    
+    for data in ar {
+        result += data
+    }
+    return result
+}
+
+/*
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+*/
+
+// 문자 입력 (정수)
+guard let arCount = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+
+// 문자열 받기
+guard let arTemp = readLine() else { fatalError("Bad input") }
+
+// 문자열 " " 로 나누어 배열로 만들기
+let ar: [Int] = arTemp.split(separator: " ").map {
+    if let arItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+        return arItem
+    } else { fatalError("Bad input") }
+}
+
+// 문자열 갯수 확인
+guard ar.count == arCount else { fatalError("Bad input") }
+
+let result = simpleArraySum(ar: ar)
+
+print(result)
+
+/*
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
+*/
