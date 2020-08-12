ff65063eab75e5b89ad5c11fdbeb233383998d4c
YongwooKim <keepingitflow@gmail.com>
2020-08-12 14:50:38 +0900

    docs: Solved Hackerrank/Strings/SuperReducedString

--- hackerrank/Strings/SuperReducedString/main.swift
+++ hackerrank/Strings/SuperReducedString/main.swift
@@ -6,7 +6,62 @@
 //  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
 //
 
+/*
+var c = MyFancyCollection([10, 20, 30, 40, 50])
+var i = c.startIndex
+while i != c.endIndex {
+    c[i] /= 5
+    i = c.index(after: i)
+}*/
+
+
 import Foundation
 
-print("Hello, World!")
+enum FinishSignal: Error {
+    case Exit
+}
 
+// Complete the superReducedString function below.
+func superReducedString(s: String) throws -> String {
+    var str = s
+    
+    do {
+        while(true) {
+            for i in str.indices {
+                if(str.index(after: i)<str.endIndex) {
+                    if(str[i] == str[str.index(after: i)]) {
+                        str.remove(at: str.index(after: i))
+                        str.remove(at: i)
+                        break
+                    }
+                } else {
+                    throw FinishSignal.Exit
+                }
+            }
+            if(str.isEmpty) {
+                throw FinishSignal.Exit
+            }
+        }
+    } catch FinishSignal.Exit {
+        if(str.isEmpty) {
+            str = "Empty String"
+        }
+        return str
+    }
+}
+
+/*
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+*/
+
+guard let s = readLine() else { fatalError("Bad input") }
+
+let result = try superReducedString(s: s)
+
+print(result)
+/*
+fileHandle.write(result.data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
+*/
