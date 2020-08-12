12963d2f0cb0dc889ded2dc1790b6aa5b463d463
YongwooKim <keepingitflow@gmail.com>
2020-08-12 21:29:34 +0900

    docs: Solved HackerRank/Strings/TwoStrings

--- hackerrank/Strings/TwoStrings/main.swift
+++ hackerrank/Strings/TwoStrings/main.swift
@@ -8,5 +8,44 @@
 
 import Foundation
 
-print("Hello, World!")
+// Complete the twoStrings function below.
+func twoStrings(s1: String, s2: String) -> String {
+    if(s1.count > s2.count) {
+        for c in s2 {
+            if(s1.range(of: String(c)) != nil) {
+                return "YES"
+            }
+        }
+    } else {
+        for c in s1 {
+            if(s2.range(of: String(c)) != nil) {
+                return "YES"
+            }
+        }
+    }
+    
+    return "NO"
+}
 
+/*
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+*/
+
+guard let q = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+for _ in 1...q {
+    guard let s1 = readLine() else { fatalError("Bad input") }
+
+    guard let s2 = readLine() else { fatalError("Bad input") }
+
+    let result = twoStrings(s1: s1, s2: s2)
+    
+    print(result)
+/*
+    fileHandle.write(result.data(using: .utf8)!)
+    fileHandle.write("\n".data(using: .utf8)!)
+*/
+}
