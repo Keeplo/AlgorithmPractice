97f120ef49cebbac4f79d9b0ddfc2ae1edb451a7
YongwooKim <keepingitflow@gmail.com>
2020-08-14 16:39:04 +0900

    docs: Try Programmers/Sorting/MaxNumber

--- programmers/Sorting/MaxNumber/main.swift
+++ programmers/Sorting/MaxNumber/main.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution(_ numbers:[Int]) -> String {
+    var result = ""
+    var arr = [String]()
+    
+    for i in numbers {
+        arr.append(String(i))
+    }
+    
+    while(!arr.isEmpty) {
+        arr.sort(by: >)
+        for i in 0..<arr.count-1 {
+            let s = arr.first!
+            if(s>arr[i]) {
+                if(arr[s.index(s.startIndex+1, offsetBy: 1)] < a[i][a[i].index(s.startIndex+1, offsetBy: 1)]) {
+                    
+                }
+            }
+        }
+        result.append(arr.first!)
+        arr.remove(at: 0)
+    }
+    
+    return result
+}
 
+let arr = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+
+let result = solution(arr)
+
+print(result)
