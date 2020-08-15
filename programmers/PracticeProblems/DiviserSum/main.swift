76eb93cbe9be19a9cd3e4b1fc163005790fec7a0
YongwooKim <keepingitflow@gmail.com>
2020-08-15 23:57:51 +0900

    docs: make project for DiviserSum

---
+++ programmers/PracticeProblems/DiviserSum/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  DiviserSum
+//
+//  Created by Yongwoo Marco on 2020/08/14.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var result = 0
+        
+    for i in 1...n {
+        if(n%i==0) {
+            print()
+            result += i }
+    }
+    return result
+}
+
+let n = Int(readLine()!)
+print(solution(n!))
