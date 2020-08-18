ebde576aaae5656f8467dceda3490075011121f9
YongwooKim <keepingitflow@gmail.com>
2020-08-18 20:55:28 +0900

    docs: Solved 연습문제/정수내림차순으로배치하기

--- programmers/PracticeProblems/AllocatingIntegers/main.swift
+++ programmers/PracticeProblems/AllocatingIntegers/main.swift
@@ -6,7 +6,35 @@
 //  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
 //
 
+// 정수 내림차순으로 배치하기
+
+// https://programmers.co.kr/learn/courses/30/lessons/12933
+
 import Foundation
 
-print("Hello, World!")
+func solution(_ n:Int64) -> Int64 {
+    var arr = [Int64]()
+    var k = n
+    
+    while(k>0) {
+        if(k/10>0) {
+            arr.append(k%10)
+            k /= 10
+        } else {
+            arr.append(k)
+            break
+        }
+    }
+    
+    arr.sort(by: >)
+    k = 0
+    for i in 0..<arr.count {
+        let tens = Int64(pow(Double(10), Double(arr.count-i-1)))
+        k += arr[i] * tens
+    }
+    return k
+}
 
+let n = Int64(readLine()!)
+
+print(solution(n!))
