bfd269c6fb9a6b59d73fde3510a4af25598eb1f6
YongwooKim <keepingitflow@gmail.com>
2020-08-20 17:23:09 +0900

    docs: 프로그래머스/문자열 다루기 기본

---
+++ programmers/PracticeProblems/BasicOfHadlingString/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  BasicOfHadlingString
+//
+//  Created by Yongwoo Marco on 2020/08/18.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+// 문자열 다루기 기본
+
+// https://programmers.co.kr/learn/courses/30/lessons/12918
+
+import Foundation
+
+func solution(_ s:String) -> Bool {
+    for i in 
+    return
+}
+
+let str = readLine()!
+
+print(solution(str))
