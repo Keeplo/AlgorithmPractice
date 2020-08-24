94b0a111d6b127aa97ae902829cf501cb7a19381
YongwooKim <keepingitflow@gmail.com>
2020-08-24 21:54:04 +0900

    docs: Solved 프로그래머스/연습문제/문자열내림차순으로배치하기

--- programmers/PracticeProblems/DescendingString/main.swift
+++ programmers/PracticeProblems/DescendingString/main.swift
@@ -6,7 +6,18 @@
 //  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
 //
 
+// 문자열 내림차순으로 배치하기
+
+// https://programmers.co.kr/learn/courses/30/lessons/12917
+
 import Foundation
 
-print("Hello, World!")
+func solution(_ s:String) -> String {
+    var result = ""
+    s.sorted(by: >).map({ result.append($0) })
+    return result
+}
 
+let str = readLine()!
+
+print(solution(str))
