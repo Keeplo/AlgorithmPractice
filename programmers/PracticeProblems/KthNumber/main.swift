5c6ed68f1577ea5018d94156e897add2f291c616
YongwooKim <keepingitflow@gmail.com>
2020-08-24 18:08:57 +0900

    docs: Solved 프로그래머스/연습문제/K번째수

--- programmers/PracticeProblems/KthNumber/main.swift
+++ programmers/PracticeProblems/KthNumber/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+    var result = [Int]()
+    
+    for command in commands {
+        let arr = array[command[0]-1...command[1]-1]
+        let array = arr.sorted()
+        let index = command[2]-1
+            
+        result.append(array[index])
+    }
+    
+    return result
+}
 
+let arr = readLine()!.components(separatedBy: " ").map({ Int($0)! })
+let n = Int(readLine()!)
+
+var commands = [[Int]]()
+
+for _ in 0..<n! {
+    let small = readLine()!.components(separatedBy: " ").map({ Int($0)! })
+    commands.append(small)
+}
+
+print(solution(arr, commands))
