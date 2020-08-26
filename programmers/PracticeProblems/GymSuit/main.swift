bbdae882a5455c01a131d6215f58e74d04814776
YongwooKim <keepingitflow@gmail.com>
2020-08-26 21:28:32 +0900

    docs: Try 프로그래머스/연습문제/체육복

---
+++ programmers/PracticeProblems/GymSuit/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  GymSuit
+//
+//  Created by Yongwoo Marco on 2020/08/25.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+    var students = [Int](repeating: 0, count: n)
+    
+    stu
+    œœœ
+    return 0
+}
+
+let n = Int(readLine()!)
+let lostArr = readLine()!.components(separatedBy: " ").map( Int($0) )
+let reserve = readLine()!.components(separatedBy: " ").map( Int($0) )
