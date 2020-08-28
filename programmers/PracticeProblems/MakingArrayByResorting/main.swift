ca9a908d060a9d495e4c8df4f48144715ccdbc09
YongwooKim <keepingitflow@gmail.com>
2020-08-28 18:42:11 +0900

    docs: Solved 프로그래머스/연습문제/자연수뒤집어배열로만들기

--- programmers/PracticeProblems/MakingArrayByResorting/main.swift
+++ programmers/PracticeProblems/MakingArrayByResorting/main.swift
@@ -6,7 +6,31 @@
 //  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
 //
 
+// 자연수 뒤집어 배열로 만들기
+
+// https://programmers.co.kr/learn/courses/30/lessons/12932
+
 import Foundation
 
-print("Hello, World!")
+/*
+func solution(_ n:Int64) -> [Int] {
+    var result = [Int]()
+    var varN = n
+    while(varN>10) {
+        if(varN/10>0) {
+            result.append(Int(varN%10))
+            varN = varN/10
+        }
+    }
+    result.append(Int(varN))
+    return result
+}
+*/
 
+func solution(_ n:Int64) -> [Int] {
+    return String(n).compactMap{ $0.hexDigitValue }.reversed()
+}
+
+let n = Int64(readLine()!)
+
+print(solution(n!))
