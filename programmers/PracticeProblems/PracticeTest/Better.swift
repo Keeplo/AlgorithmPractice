26f1143fbf22d9d56dcc527881cb853ccabeb062
YongwooKim <keepingitflow@gmail.com>
2020-08-28 15:26:38 +0900

    docs: Solved 프로그래머스/연습문제/직사각형별찍기

---
+++ programmers/PracticeProblems/PracticeTest/Better.swift
@@ -0,0 +1,9 @@
+//
+//  Better.swift
+//  PracticeTest
+//
+//  Created by Yongwoo Marco on 2020/08/27.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
---
+++ programmers/PracticeProblems/PutStars/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  PutStars
+//
+//  Created by Yongwoo Marco on 2020/08/28.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+for _ in 0..<b {
+    for _ in 0..<a {
+        print("*", terminator: "")
+    }
+    print()
+}
