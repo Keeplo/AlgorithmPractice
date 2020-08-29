973bf1542425a61d8887b845d65bda6c6420c61f
YongwooKim <keepingitflow@gmail.com>
2020-08-29 17:49:48 +0900

    docs: Solved 프로그래머스/연습문제/정수제곱근판별

---
+++ programmers/PracticeProblems/FindingIntegerSquareRoot/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  FindingIntegerSquareRoot
+//
+//  Created by Yongwoo Marco on 2020/08/28.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+// 정수 제곱근 판별
+
+// https://programmers.co.kr/learn/courses/30/lessons/12934
+
+import Foundation
+
+func solution(_ n:Int64) -> Int64 {
+    return sqrt(Double(n)) == Double(Int(sqrt(Double(n)))) ? Int64(pow(sqrt(Double(n))+1, 2)) : -1
+}
+
+let n = Int64(readLine()!)
+
+print(solution(n!))
