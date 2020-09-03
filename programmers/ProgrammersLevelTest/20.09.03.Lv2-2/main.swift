b76ace8b8ded9845369c8b05476a03324053305e
YongwooKim <keepingitflow@gmail.com>
2020-09-04 00:07:34 +0900

    docs: 프로그래머스/레벨테스트/2번

--- programmers/ProgrammersLevelTest/20.09.03.Lv2-2/main.swift
+++ programmers/ProgrammersLevelTest/20.09.03.Lv2-2/main.swift
@@ -8,5 +8,24 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution(_ str1:String, _ str2:String) -> Int {
+    var arr1 = [String]()
+    var arr2 = [String]()
+    
+    for a in 0..<str1.count-1 {
+        let com = (str1 as NSString).substring(with: NSRange(location:a, length:2))
+        arr1.append(com)
+    }
+    for b in 0..<str2.count-1 {
+        let com = (str1 as NSString).substring(with: NSRange(location:b, length:2))
+        arr2.append(com)
+    }
+    let array1 = arr1.filter( String($0). )
+    
+    return 0
+}
 
+let str1 = readLine()!.lowercased()
+//let str2 = readLine()!.lowercased()
+
+print(solution(str1, str1))
