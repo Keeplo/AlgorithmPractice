fe53686c21b3eb39e81848538e26b729ac381138
YongwooKim <keepingitflow@gmail.com>
2020-09-04 17:14:51 +0900

    docs: 프로그래머스/연습문제/콜라츠

---
+++ programmers/PracticeProblems/Collatz/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Collatz
+//
+//  Created by Yongwoo Marco on 2020/09/04.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ num:Int) -> Int {
+    
+    return 0
+}
