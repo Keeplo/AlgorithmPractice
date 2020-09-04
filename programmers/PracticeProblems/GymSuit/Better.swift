0552dcfbf6510599fde941d7118cb5d5b2e8330a
YongwooKim <keepingitflow@gmail.com>
2020-09-04 16:59:22 +0900

    docs: 프로그래머스/연습문제/체육복

---
+++ programmers/PracticeProblems/GymSuit/Better.swift
@@ -0,0 +1,9 @@
+//
+//  Better.swift
+//  GymSuit
+//
+//  Created by Yongwoo Marco on 2020/09/04.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
--- programmers/PracticeProblems/GymSuit/main.swift
+++ programmers/PracticeProblems/GymSuit/main.swift
@@ -10,10 +10,34 @@ import Foundation
 
 func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
     var students = [Int](repeating: 0, count: n)
+    var r = reserve
     
-    return 0
+    // 여분 가져왔는데 잃어버린 학생 제외하기
+    let newLost = lost.filter({ (index: Int) -> Bool in
+        if(r.contains(index)) {
+            r = r.filter({ $0 != index })
+            return false
+        } else {
+            return true
+        }
+    })
+    
+    // 잃어버린 학생 양옆에 여분있는지 확인
+    for i in newLost {
+        if(r.contains(i-1)) {           // -1 번 학생 여분 있음
+            r = r.filter({ $0 != i-1 })
+        } else if(r.contains(i+1)) {    // +1 번 학생 여분 있음
+            r = r.filter({ $0 != i+1 })
+        } else {                        // 여분있는 학생 옆에 없어서 참석 불가
+            students[i-1] = -1
+        }
+    }
+    
+    return n+students.reduce(0){ $0+$1 }
 }
 
 let n = Int(readLine()!)
-let lostArr = readLine()!.components(separatedBy: " ").map( Int($0) )
-let reserve = readLine()!.components(separatedBy: " ").map( Int($0) )
+let lostArr = readLine()!.components(separatedBy: " ").map( {Int($0)!} )
+let reserve = readLine()!.components(separatedBy: " ").map( {Int($0)!} )
+
+print(solution(n!, lostArr, reserve))
