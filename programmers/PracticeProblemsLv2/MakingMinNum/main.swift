5adc7ed9347e0622e66e09fc149241ed6f66ff20
YongwooKim <keepingitflow@gmail.com>
2020-09-05 11:30:21 +0900

    docs: Solved 프로그래머스/연습문제/최솟값만들기

---
+++ programmers/PracticeProblemsLv2/MakingMinNum/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  MakingMinNum
+//
+//  Created by Yongwoo Marco on 2020/09/05.
+//  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
+//
+
+// 최솟값 만들기
+
+// https://programmers.co.kr/learn/courses/30/lessons/12941
+
+import Foundation
+
+func solution(_ A:[Int], _ B:[Int]) -> Int {
+    let a = A.sorted(by: >)
+    let b = B.sorted(by: <)
+    
+    return (0..<A.count).reduce(0, { $0 + (a[$1]*b[$1]) })
+}
+
+let arr1 = readLine()!.components(separatedBy: " ").map({ Int($0)! })
+let arr2 = readLine()!.components(separatedBy: " ").map({ Int($0)! })
+print(solution(arr1, arr2))
