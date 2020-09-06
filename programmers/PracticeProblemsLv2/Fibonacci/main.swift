5b8604661f6172c7e39932a494e38cb9aa036098
YongwooKim <keepingitflow@gmail.com>
2020-09-06 23:02:03 +0900

    docs: Updated files

--- programmers/PracticeProblemsLv2/Fibonacci/main.swift
+++ programmers/PracticeProblemsLv2/Fibonacci/main.swift
@@ -6,7 +6,21 @@
 //  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
 //
 
+// 피보나치 수
+
+// https://programmers.co.kr/learn/courses/30/lessons/12945
+
 import Foundation
 
-print("Hello, World!")
+func solution(_ n:Int) -> Int {
+    var f = [0, 1]
+    for i in 2...n {
+        f.append((f[i-1]+f[i-2])%1234567)
+    }
+    
+    return f[n]
+}
 
+let n = Int(readLine()!)
+
+print(solution(n!))
