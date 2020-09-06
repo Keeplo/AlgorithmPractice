ca64c8014b26bc60a48d7e85fb0d43fbc2cd79b4
YongwooKim <keepingitflow@gmail.com>
2020-09-07 00:41:01 +0900

    docs: Uploaded the post

--- programmers/PracticeProblems/CaesarPassword/main.swift
+++ programmers/PracticeProblems/CaesarPassword/main.swift
@@ -12,6 +12,7 @@
 
 import Foundation
 
+/*
 func solution(_ s:String, _ n:Int) -> String {
     var str = ""
     
@@ -38,7 +39,31 @@ func solution(_ s:String, _ n:Int) -> String {
     }
     return str
 }
+*/
 
+func solution(_ s:String, _ n:Int) -> String {
+    var str = ""
+    
+    let ascii = (a: UInt8(65), z: UInt8(90), A: UInt8(97), Z: UInt8(122))
+    
+    for c in s {
+        if(c == " ") {
+            str.append(c)
+        } else {
+            let char = Character(extendedGraphemeClusterLiteral: c)
+            var sum = char.asciiValue! + UInt8(n)
+            
+            if(char.asciiValue! <= ascii.z && sum > ascii.z) {
+                sum = (ascii.a-1) + (sum-ascii.z)
+            } else if(char.asciiValue! >= ascii.A && sum>ascii.Z)  {
+                sum = (ascii.A-1) + (sum-ascii.Z)
+            }
+            
+            str.append( Character(UnicodeScalar(sum)) )
+        }
+    }
+    return str
+}
 
 let str = readLine()
 let n = Int(readLine()!)
