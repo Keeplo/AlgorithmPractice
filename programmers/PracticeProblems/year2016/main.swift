fdd7d018efffa9d3e8e5ab5cdd0f37b95cc763e6
YongwooKim <keepingitflow@gmail.com>
2020-09-14 23:45:05 +0900

    docs: Uploaded the post

--- programmers/PracticeProblems/year2016/main.swift
+++ programmers/PracticeProblems/year2016/main.swift
@@ -6,8 +6,15 @@
 //  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
 //
 
+// 2016년
+
+// https://programmers.co.kr/learn/courses/30/lessons/12901
+
+// https://keeplo.tistory.com/116
+
 import Foundation
 
+/*
 func solution(_ a:Int, _ b:Int) -> String {
     let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI" , "SAT"]
     let months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -22,8 +29,15 @@ func solution(_ a:Int, _ b:Int) -> String {
     
     return days[index]
 }
+ */
+func solution(_ a:Int, _ b:Int) -> String {
+    let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI" , "SAT"]
+    let months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    return days[ (5 + months[0..<a-1].reduce(0, +) + b-1 )%7 ]
+}
 
 let months = Int(readLine()!)
 let days = Int(readLine()!)
 
 print(solution(months!, days!))
+
