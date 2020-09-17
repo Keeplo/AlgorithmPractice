13f25ba7bf4d730e5ac29d547f1f9dcb6a51a019
YongwooKim <keepingitflow@gmail.com>
2020-09-17 13:40:39 +0900

    docs: Uploaded the post

--- programmers/PracticeProblems/KthNumber/main.swift
+++ programmers/PracticeProblems/KthNumber/main.swift
@@ -6,8 +6,14 @@
 //  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
 //
 
-import Foundation
+// K번째수
 
+// https://programmers.co.kr/learn/courses/30/lessons/42748
+
+// https://keeplo.tistory.com/121
+
+import Foundation
+/*
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
     var result = [Int]()
     
@@ -31,5 +37,13 @@ for _ in 0..<n! {
     let small = readLine()!.components(separatedBy: " ").map({ Int($0)! })
     commands.append(small)
 }
+*/
 
+func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+    return commands.map({ array[$0[0]-1...$0[1]-1].sorted(by: < )[$0[2]-1] })
+}
+
+let arr = [1, 5, 2, 6, 3, 7, 4]
+let commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+    
 print(solution(arr, commands))
