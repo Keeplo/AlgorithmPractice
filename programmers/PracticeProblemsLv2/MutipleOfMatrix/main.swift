9da4eded923a105fc3a560509559df4e2c80ca72
YongwooKim <keepingitflow@gmail.com>
2020-09-21 11:27:52 +0900

    docs: Uploaded the post

--- programmers/PracticeProblemsLv2/MutipleOfMatrix/main.swift
+++ programmers/PracticeProblemsLv2/MutipleOfMatrix/main.swift
@@ -6,30 +6,36 @@
 //  Copyright © 2020 Yongwoo Marco Kim. All rights reserved.
 //
 
+// 행렬의 곱셈
+
+// https://programmers.co.kr/learn/courses/30/lessons/12949
+
+//
+
 import Foundation
 
 func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-    
-    var result = [[Int]]()
-    
-    for i in arr1 {
+    return arr1.map({ (a1: [Int]) -> [Int] in
         var tempArr = [Int]()
-        for j in 0..<arr2[0].count {
-            var tempNum = 0
-            
-            for k in 0..<arr2.count {
-                tempNum += i[k]*arr2[k][j]
-            }
-            tempArr.append(tempNum)
+        
+        for i in 0..<arr2[0].count {
+            var index = -1
+            tempArr.append(arr2.reduce(0, { (before: Int, present: [Int]) -> Int in
+                index += 1
+                return before + (a1[index]*present[i])
+                })
+            )
         }
-        result.append(tempArr)
-    }
-    
-    return result
+        return tempArr
+    })
 }
 
-let arr1 = [[1, 4], [3, 2], [4, 1]]
-let arr2 = [[3, 3], [3, 3]]
+let arr1 = [[1, 4],
+            [3, 2],
+            [4, 1]]
+
+let arr2 = [[3, 3],
+            [3, 3]]
 
 print(solution(arr1, arr2))
     
