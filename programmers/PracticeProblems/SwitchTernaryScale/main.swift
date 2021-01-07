9bd049abdc04a310c0f764c8098f151afef20a14
YongwooKim <keepingitflow@gmail.com>
2021-01-07 20:36:46 +0900

    docs: Posted 3진법 뒤집기

--- programmers/PracticeProblems/SwitchTernaryScale/main.swift
+++ programmers/PracticeProblems/SwitchTernaryScale/main.swift
@@ -6,31 +6,42 @@
 //  Copyright © 2021 Yongwoo Marco Kim. All rights reserved.
 //
 
+// 3진법 뒤집기
+
+// https://programmers.co.kr/learn/courses/30/lessons/68935
+// https://keeplo.tistory.com/163
+
 import Foundation
 
+//func solution(_ n:Int) -> Int {
+//    var divide = n
+//    var ternary = [Int]()
+//
+//    while(divide>0) {
+//        if divide<3 {
+//            ternary.insert(divide, at: 0)
+//            divide = 0
+//        } else {
+//            ternary.insert(divide%3, at: 0)
+//            divide /= 3
+//        }
+//    }
+//
+//    for index in 0..<ternary.count {
+//        ternary[index] *= Int(pow(Double(3), Double(index)))
+//    }
+//
+//    return ternary.reduce(0, +)
+//}
+
 func solution(_ n:Int) -> Int {
-    var divide = n
-    var ternary = [Int]()
+    let ternary = String(n, radix: 3)
+    let result = Int(String(ternary.reversed()),radix:3)!
 
-    while(divide>0) {
-        if divide<3 {
-            ternary.insert(divide, at: 0)
-            divide = 0
-        } else {
-            ternary.insert(divide%3, at: 0)
-            divide /= 3
-        }
-    }
-
-    for index in 0..<ternary.count {
-        ternary[index] *= Int(pow(Double(3), Double(index)))
-    }
-
-    return ternary.reduce(0, +)
+    return result
 }
 
 
 let n = 45
 
 print(solution(n))
-
