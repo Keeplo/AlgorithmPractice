dff17b97d8ac516f2673436acfbaac007dfe72de
YongwooKim <keepingitflow@gmail.com>
2021-01-07 20:08:03 +0900

    docs: 3진법 뒤집기 풀이 완료

--- programmers/PracticeProblems/SwitchTernaryScale/main.swift
+++ programmers/PracticeProblems/SwitchTernaryScale/main.swift
@@ -8,33 +8,27 @@
 
 import Foundation
 
-//func solution(_ n:Int) -> Int {
-//    var divide = n
-//    var ternary = [Int]()
-//
-//    while(divide>0) {
-//        if divide<3 {
-//            ternary.insert(divide, at: 0)
-//            divide = 0
-//        } else {
-//            ternary.insert(divide%3, at: 0)
-//            divide /= 3
-//        }
-//    }
-//
-//    for index in 0..<ternary.count {
-//        ternary[index] *= Int(pow(Double(3), Double(index)))
-//    }
-//
-//    return ternary.reduce(0, +)
-//}
-
 func solution(_ n:Int) -> Int {
-    let ternary = String(n, radix: 3)
-    let result = Int(String(ternary.reversed()),radix:3)!
-    
-    return result
+    var divide = n
+    var ternary = [Int]()
+
+    while(divide>0) {
+        if divide<3 {
+            ternary.insert(divide, at: 0)
+            divide = 0
+        } else {
+            ternary.insert(divide%3, at: 0)
+            divide /= 3
+        }
+    }
+
+    for index in 0..<ternary.count {
+        ternary[index] *= Int(pow(Double(3), Double(index)))
+    }
+
+    return ternary.reduce(0, +)
 }
+
 
 let n = 45
 
