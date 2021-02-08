d932a4c0ffabb3a31780b6e91d0bf4350d2ddbc2
YongwooKim <keepingitflow@gmail.com>
2021-02-08 17:51:36 +0900

    Docs: Posted This Problem

--- programmers/PracticeProblemsLv2/CorrectBracket/main.swift
+++ programmers/PracticeProblemsLv2/CorrectBracket/main.swift
@@ -6,7 +6,34 @@
 //  Copyright © 2021 Yongwoo Marco Kim. All rights reserved.
 //
 
+// 올바른 괄호
+
+// https://programmers.co.kr/learn/courses/30/lessons/12909
+// https://keeplo.tistory.com/176
+
 import Foundation
 
-print("Hello, World!")
+func solution(_ s:String) -> Bool {
+    var stack = [Character]()
+    
+    for c in s {
+        if c == Character("(") {
+            stack.append(c)
+        } else {
+            if c == Character(")"), stack.isEmpty == true {
+                return false
+            }
+            _ = stack.popLast()
+        }
+    }
 
+    return stack.count > 0 ? false : true
+}
+
+
+let s = "(())()"
+// let s = "(())()"
+// let s = ")()("
+// let s = "(()("
+
+print(solution(s))
