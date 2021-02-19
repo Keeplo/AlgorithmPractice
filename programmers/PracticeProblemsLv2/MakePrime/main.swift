5ccdabe30d78f22b014f72dbe232d95b1fbda426
YongwooKim <keepingitflow@gmail.com>
2021-02-19 21:58:49 +0900

    Docs: Solved this problem

---
+++ programmers/PracticeProblemsLv2/MakePrime/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MakePrime
+//
+//  Created by Yongwoo Marco on 2021/02/19.
+//  Copyright © 2021 Yongwoo Marco Kim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
