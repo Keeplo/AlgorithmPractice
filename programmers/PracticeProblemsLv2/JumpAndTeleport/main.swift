28aaccbb0d576bb3318e5ff426c6453b36a3df1a
YongwooKim <keepingitflow@gmail.com>
2021-02-20 17:42:54 +0900

    Docs: Posted this problem

--- programmers/PracticeProblemsLv2/JumpAndTeleport/main.swift
+++ programmers/PracticeProblemsLv2/JumpAndTeleport/main.swift
@@ -6,7 +6,29 @@
 //  Copyright © 2021 Yongwoo Marco Kim. All rights reserved.
 //
 
+// 점프와 순간이동
+
+// https://programmers.co.kr/learn/courses/30/lessons/12980
+// https://keeplo.tistory.com/188
+
 import Foundation
 
-print("Hello, World!")
+func solution(_ n:Int) -> Int {
+    var distance = n, cost = 0
+    
+    while distance != 0 {
+        if distance % 2 == 1 {
+            cost += 1
+        }
+        
+        distance /= 2
+    }
+    
+    return cost
+}
 
+//let n = 5       // 2
+//let n = 6       // 2
+let n = 5000    // 5
+
+print(solution(n))
