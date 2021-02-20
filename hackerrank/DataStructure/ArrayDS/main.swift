46ee3b610f04301c15f659a91d53c7f56007402f
YongwooKim <keepingitflow@gmail.com>
2021-02-20 20:13:41 +0900

    Docs: Solved 'ArrayDS'

---
+++ hackerrank/DataStructure/ArrayDS/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  ArrayDS
+//
+//  Created by Yongwoo Marco on 2021/02/20.
+//
+
+import Foundation
+
+func reverseArray(a: [Int]) -> [Int] {
+
+    return a.reversed()
+
+}
+
+
+let a = [1, 4, 3, 2]
+
+print(reverseArray(a: a))
