4007584be785b5de08e8a11b629927cfaea23512
YongwooKim <keepingitflow@gmail.com>
2021-03-01 19:01:17 +0900

    Docs : Tried but not solved yet

--- programmers/PracticeProblemsLv2/CorrectSquare/main.swift
+++ programmers/PracticeProblemsLv2/CorrectSquare/main.swift
@@ -14,16 +14,37 @@
 import Foundation
 
 func solution(_ w:Int, _ h:Int) -> Int64{
-    var answer:Int64 = 1
-    var temp = (Int64(w), Int64(h))
+    var m:Int64  = 1, W = Int64(w), H = Int64(h)
     
+    if w == h {
+        return Int64(w * (w-1))
+    }
     
+    while W%2 == 0, H%2 == 0 {
+        m *= 2
+        W = W/2
+        H = H/2
+    }
     
-    return answer
+    print("W \(W), H \(H), w \(w), h \(h), m \(m)")
+    
+    if W > H {
+        return H * m * (Int64(w) - H)
+    } else {
+        return W * m * (Int64(h) - W)
+    }
+    
+//    answer = Int64(w * h) - (W * W) * m
+//    answer = Int64(w * h) - (H * H) * m
+    
+//    W * m * (Int64(h) - W)
+//    H * m * (Int64(w) - H)       Int64(w * H * m) - (H * H) * m
+    
+//    
 }
 
 
-let w = 8
-let h = 12
+let w = 4
+let h = 3
 
 print(solution(w, h))
