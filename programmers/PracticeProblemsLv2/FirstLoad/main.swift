a074c09b34642034ab73faafefe1322b5ca06fd0
YongwooKim <keepingitflow@gmail.com>
2021-03-05 23:09:12 +0900

    Docs : Posted "방문 길이"

--- programmers/PracticeProblemsLv2/FirstLoad/main.swift
+++ programmers/PracticeProblemsLv2/FirstLoad/main.swift
@@ -9,14 +9,14 @@
 // 방문 길이
 
 // https://programmers.co.kr/learn/courses/30/lessons/49994
-//
+// https://keeplo.tistory.com/197
 
 import Foundation
 
 
 func solution(_ dirs:String) -> Int {
     let points = ["U":(1, 0), "D":(-1,0), "R":(0, 1), "L":(0, -1)] // (y, x)
-    var past = [((Int, Int), (Int, Int))](), c = (0, 0)
+    var past = [(Int, Int, Int, Int)](), c = (0, 0)
     
     for moves in dirs {
         let m = String(moves)
@@ -29,8 +29,8 @@ func solution(_ dirs:String) -> Int {
         let nextX = c.1 + p.1
         
         if nextX <= 5, nextX >= -5, nextY <= 5, nextY >= -5 {
-            if past.filter({ ($0.0 == c && $0.1 == (nextY, nextX)) || ($0.1 == c && $0.0 == (nextY, nextX)) }).count == 0 {
-                past.append( (c, (c.0 + p.0, c.1 + p.1)) )
+            if past.filter({ $0 == (c.0, c.1, nextY, nextX) || $0 == (nextY, nextX, c.0, c.1) }).count == 0 {
+                past.append( (c.0, c.1, nextY, nextX) )
             }
             c = (c.0 + p.0, c.1 + p.1)
         }
