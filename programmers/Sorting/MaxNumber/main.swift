0a56423bc3f832aaa2599dd6973e66db288eb03d
YongwooKim <keepingitflow@gmail.com>
2021-03-07 20:00:48 +0900

    Docs : Solved "가장 큰수"

--- programmers/Sorting/MaxNumber/main.swift
+++ programmers/Sorting/MaxNumber/main.swift
@@ -9,17 +9,58 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> String {
-    let strNums = numbers.map({ String($0) })
-    var maxNum = ""
+    var strNums = numbers.map({ String($0) }).sorted(by: { $0+$1 > $1+$0 })
     
-    
-        
-    return String(maxNum)
+    return strNums.first == "0" ? "0" : strNums.joined()
 }
 
 //let numbers = [6, 10, 2]            // "6210"
-let numbers = [3, 30, 34, 5, 9]     // "9534330"
+//let numbers = [3, 30, 34, 5, 9]     // "9534330"
+let numbers = [0, 0, 0]
 
 print(solution(numbers))
 
 
+//func solution(_ numbers:[Int]) -> String {
+//    var strNums = numbers.map({ String($0) }).sorted(by: >)
+//    var maxNum = "", maxExtra = ""
+//
+//    //가능한 모든 경우의 수
+//    func permuteWirth(_ a: [String], _ n: Int) {
+//        if n == 0 {
+//            let num = a.joined()
+//            if maxExtra < num {
+//                maxExtra = num
+//            }
+//        } else {
+//            var a = a
+//            permuteWirth(a, n - 1)
+//            for i in 0..<n {
+//                a.swapAt(i, n)
+//                permuteWirth(a, n - 1)
+//                a.swapAt(i, n)
+//            }
+//        }
+//    }
+//
+//    while strNums.count > 0 {
+//        let s = strNums.removeFirst()
+//
+//        if s.count > 1, strNums.filter({ $0.first == s.first }).count > 0 {
+//            print(s)
+//            var extraS = strNums.filter({ $0.first == s.first  })
+//            let withoutS = strNums.filter({ $0.first != s.first  })
+//
+//            extraS.insert(s, at: 0)
+//
+//            permuteWirth(extraS, extraS.count - 1)
+//            strNums = withoutS
+//            maxNum += maxExtra
+//            maxExtra = ""
+//        } else {
+//            maxNum.append(s)
+//        }
+//    }
+//
+//    return String(maxNum)
+//}
