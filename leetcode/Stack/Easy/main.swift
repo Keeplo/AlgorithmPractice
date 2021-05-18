68ee051cea950ad8f840e2865e4330c76056e368
YongwooKim <keepingitflow@gmail.com>
2021-05-18 19:07:34 +0900

    Docs : Solved leetcode/Stack/Easy/1598, 1544, 844, 155, 20

--- leetcode/Stack/Easy/main.swift
+++ leetcode/Stack/Easy/main.swift
@@ -118,3 +118,126 @@
 //print(nextGreaterElement([4,1,2], [1,3,4,2]))   // [-1, 3, -1]
 //print(nextGreaterElement([2,4], [1,2,3,4]))     // [3, -1]
 //print(nextGreaterElement([4,1,2], [1,2,3,4]))   // [-1, 2, 3]
+
+// MARK: --- 21.05.14
+// 1598. Crawler Log Folder
+//func minOperations(_ logs: [String]) -> Int {
+//    var stack = [String]()
+//
+//    for s in logs {
+//        if s == "../" {
+//            stack.popLast()
+//        } else if s != "./" {
+//            stack.append(s)
+//        }
+//    }
+//    return stack.count
+//}
+//print(minOperations(["d1/","d2/","../","d21/","./"]))       // 2
+//print(minOperations(["d1/","d2/","./","d3/","../","d31/"])) // 3
+//print(minOperations(["d1/","../","../","../"]))             // 0
+//print(minOperations(["d1/","d2/","../","d21/","./"]))       // 2
+
+// 1544. Make The String Great
+//func makeGood(_ s: String) -> String {
+//    var arr = s.map{String($0)}, result = [String]()
+//
+//    while !arr.isEmpty {
+//        if arr.count > 1, arr[0] != arr[1], arr[0].uppercased() == arr[1] || arr[0].lowercased() == arr[1] {
+//            arr.removeFirst()
+//            arr.removeFirst()
+//            arr = result + arr
+//            result = []
+//        } else {
+//            result.append(arr.removeFirst())
+//        }
+//    }
+//
+//    return result.joined()
+//}
+//print(makeGood("leEeetcode"))   // "leetcode"
+//print(makeGood("abBAcC"))       // "abBAcC"
+//print(makeGood("s"))            // "s"
+
+// 844. Backspace String Compare
+//func backspaceCompare(_ s: String, _ t: String) -> Bool {
+//    var ss = [Character](), ts = [Character]()
+//
+//    for c in s {
+//        if c == "#" {
+//            ss.popLast()
+//        } else {
+//            ss.append(c)
+//        }
+//    }
+//    for c in t {
+//        if c == "#" {
+//            ts.popLast()
+//        } else {
+//            ts.append(c)
+//        }
+//    }
+//
+//    return ss == ts ? true : false
+//}
+//print(backspaceCompare("ab#c", "ab#c")) // true
+//print(backspaceCompare("ab##", "c#d#")) // true
+//print(backspaceCompare("a##c", "#a#c")) // true
+//print(backspaceCompare("a#c", "b"))     // false
+
+// 155. Min Stack
+//class MinStack {
+//    var arr = [Int]()
+//    /** initialize your data structure here. */
+//    init() {
+//        arr = []
+//    }
+//
+//    func push(_ val: Int) {
+//        arr.append(val)
+//    }
+//
+//    func pop() {
+//        arr.removeLast()
+//    }
+//
+//    func top() -> Int {
+//        return arr.last!
+//    }
+//
+//    func getMin() -> Int {
+//        return arr.min()!
+//    }
+//}
+
+// MARK: Important
+// 20. Valid Parentheses
+//func getOffside(_ c: Character) -> Character {
+//    switch c {
+//    case "(":
+//        return ")"
+//    case "{":
+//        return "}"
+//    default:
+//        return "]"
+//    }
+//}
+//func isValid(_ s: String) -> Bool {
+//    var stack = [Character]()
+//
+//    for c in s {
+//        if let last = stack.last, getOffside(last) == c {
+//            stack.removeLast()
+//        } else {
+//            stack.append(c)
+//        }
+//    }
+//    return stack.isEmpty
+//}
+//print(isValid("()"))        // true
+//print(isValid("()[]{}"))    // true
+//print(isValid("(]"))        // false
+//print(isValid("([)]"))      // false
+//print(isValid("{[]}"))      // true
+
+// MARK: --- 21.05.
